6013f83274d65f535895edf6628d2a54b3efd7b8
unbearabletruth <antaarty@gmail.com>
2023-05-05 16:47:36 +0700

    setting up classes

---
+++ script.js
@@ -0,0 +1,52 @@
+class Node{
+    constructor(value, next){
+        this.value = null;
+        this.next = null;
+    }
+}
+
+class LinkedList{
+    constructor(){
+        this.list = [];
+    }
+
+    append(value){
+
+    }
+
+    prepend(value){
+
+    }
+
+    size(){
+
+    }
+
+    head(){
+
+    }
+
+    tail(){
+
+    }
+
+    at(index){
+
+    }
+
+    pop(){
+
+    }
+
+    contains(value){
+
+    }
+
+    find(value){
+
+    }
+
+    toString(){
+        
+    }
+}
