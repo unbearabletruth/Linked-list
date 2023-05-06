6f9a7760526ab17cc3a9423a8dae86e8dc23ac85
unbearabletruth <antaarty@gmail.com>
2023-05-06 13:53:45 +0700

    some other methods

--- script.js
+++ script.js
@@ -21,7 +21,6 @@ class LinkedList {
         }
         current.next = node;
     }
-    
 
     prepend(value){
         let node = new Node(value);
@@ -44,28 +43,61 @@ class LinkedList {
     }
 
     getHead(){
-
+        return `Head: ( ${this.head.value} )`;
     }
 
     getTail(){
-
+        let node = this.head;
+        while (node.next){
+            node = node.next;
+        }
+        return `Tail: ( ${node.value} )`;
     }
 
     at(index){
-
+        let node = this.head;
+        let nodeIndex = 0;
+        while (node){
+            if (nodeIndex === index){
+                return `at index ${index}: ( ${node.value} )`;
+            }
+            node = node.next;
+            nodeIndex++;
+        }
+        return `no element at such index: ${index}`;
     }
 
     pop(){
-
+        /*let size = this.size();
+        let penultimate = this.at(size - 2);
+        console.log(penultimate)
+        penultimate.next = null;*/
     }
 
     contains(value){
-
+        let node = this.head;
+        while(node){
+            if(node.value === value){
+                return true;
+            }
+            node = node.next;
+        }
+        return false;
     }
 
     find(value){
-
+        let node = this.head;
+        let index = 0;
+        while(node){
+            if(node.value === value){
+                return `found at index: ${index}`;
+            }
+            node = node.next;
+            index++;
+        }
+        return null;
     }
+
     toString(){
         let string = "";
         let current = this.head;
@@ -85,6 +117,11 @@ list.append(9);
 list.prepend(3);
 list.prepend(2);
 console.log(list.size());
-
+console.log(list.getHead())
+console.log(list.getTail())
+console.log(list.at(5))
+list.pop()
+console.log(list.contains(10))
+console.log(list.find(10))
 list.toString()
 
